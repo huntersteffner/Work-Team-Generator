0cfc0687f4dd6944d366faf92f777f178dd5cdf4
huntersteffner <huntersteffner@gmail.com>
2022-07-15 19:19:02 -0400

    Set up template for project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const inquirer = require('inquirer')
+const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'employeeType',
+            message: 'What is the name of the manager for this team?'
+        }
+    ])
